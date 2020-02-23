e2070075f5dfdf956875a116ed3bd9568be6f31e
Alex <gistrec@mail.ru>
2020-02-23 15:51:36 +0700

    Test CodeCov

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-///Returns the value of x multiplied by 2, except for 42, which is multiplied by one
+// Returns the value of x multiplied by 2, except for 42, which is multiplied by one
 int do_magic(const int x){
     if (x == 42) {
         return 42;
